5b627f17bf0615a34e05ed9d38f04c9e71bd0e0a
marcelotozzi <marcelotozzidelima@gmail.com>
2015-04-17 22:16:39 -0300

    melhoria nos testes

--- src/test/resources/data.sql
+++ src/test/resources/data.sql
@@ -1,2 +1,2 @@
 insert into ADDRESS(id,street,number,complement,zipcode,district, city, state) values (1, 'Av. Mutinga', '5452', NULL ,'05110000', 'Jd. Santo Elias','São Paulo', 'SP');
-insert into ADDRESS(id,street,number,complement,zipcode,district, city, state) values (2, 'Av. Mutinga', '5458', NULL ,'05110000', 'Jd. Santo Elias','São Paulo', 'SP');
+insert into ADDRESS(id,street,number,complement,zipcode,district, city, state) values (2, 'Rua Moacir Salles Davila', '297', NULL ,'06288020', 'Jd. Davila','Osasco', 'SP');
